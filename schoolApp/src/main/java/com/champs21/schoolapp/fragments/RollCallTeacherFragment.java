c92320fcd29eaf86d02af089974dfef7bfb27d0f
Taufiq Ovi <ovioviovi@gmail.com>
2015-03-23 17:17:19 +0600

    1. Normal changes.

--- schoolApp/src/main/java/com/champs21/schoolapp/fragments/RollCallTeacherFragment.java
+++ schoolApp/src/main/java/com/champs21/schoolapp/fragments/RollCallTeacherFragment.java
@@ -331,7 +331,7 @@ public class RollCallTeacherFragment extends Fragment implements LeaveApplicatio
 	AsyncHttpResponseHandler getStudentHandler = new AsyncHttpResponseHandler() {
 		public void onFailure(Throwable arg0, String arg1) {
 			pbLayout.setVisibility(View.GONE);
-			Log.e("error", arg1);
+			//Log.e("error", arg1);
 		};
 
 		public void onStart() {
