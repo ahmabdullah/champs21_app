5d2df3874e1eaad87765ea98c332a1a6ceaf8ec9
Tasvir <aurgho.350@gmail.com>
2015-11-08 13:40:39 +0600

    Added Classtune Registration Activity under Classtune package

---
+++ schoolApp/src/main/java/com/champs21/schoolapp/classtune/ClassTuneRegistrationActivity.java
@@ -0,0 +1,9 @@
+package com.champs21.schoolapp.classtune;
+
+import android.app.Activity;
+
+/**
+ * Created by Tasvir on 11/8/2015.
+ */
+public class ClassTuneRegistrationActivity extends Activity{
+}
